673cf3a50ed2623fb09967bd39ab532730c8628d
root <root@RyanGSamsung.localdomain>
2022-02-01 13:02:38 -0700

    added Homework0

---
+++ Homework0/hw0.js
@@ -0,0 +1,14 @@
+console.log("Hello World")
+var cow = 2+3
+console.log(cow)
+
+
+function mult(num1, num2){
+    let result = num1 * num2
+    console.log(result)
+}
+
+
+mult(2,9)
+
+
--- Homework0/readme.txt
+++ Homework0/readme.txt
@@ -1,1 +1,7 @@
 Hello!
+Different Changed
+bmsdbjsbkdjn
+klsndkvjbsd
+skbdvmh 
+dc mn x
+xmdv mxn Chang
